cd456615e8e084a23eab0e7e5c9ffdac53e7c4d1
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-16 03:24:28 -0400

    setup server and test connections to DB

---
+++ Backend/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Load environment variables
+dotenv.config();
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log('MongoDB connected successfully');
+    })
+    .catch(err => {
+        console.error(`MongoDB connection error: ${err}`);
+    });
+
+// Connection Events
+const db = mongoose.connection;
+
+db.on('connected', () => {
+    console.log('Mongoose default connection is open');
+});
+
+db.on('error', (err) => {
+    console.error(`Mongoose default connection has occurred ${err} error`);
+});
+
+db.on('disconnected', () => {
+    console.log('Mongoose default connection is disconnected');
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello, Workout Tracker!');
+});
+
+app.listen(PORT, () => {
+    console.log('The Data Flows Through You!');
+    console.log(`Author: ${process.env.DEV}`);
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+

7907de71334650333df7650727bd7c0bba9acd98
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-16 04:45:32 -0400

    Refactor WorkoutSchema to include enhanced Exercise schema

--- Backend/models/Workout.js
+++ Backend/models/Workout.js
@@ -1,15 +1,47 @@
 const mongoose = require('mongoose');
 
-const WorkoutSchema = new mongoose.Schema({
+const ExerciseSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
     },
-    exercises: [
-        {
-            type: String,
-        },
-    ],
+    sets: {
+        type: Number,
+        default: 0,
+    },
+    reps: {
+        type: Number,
+        default: 0,
+    },
+    duration: {
+        type: String, // Format: "minutes:seconds"
+        default: "0:00",
+    },
+    weight: {
+        type: Number, // In pounds
+        default: 0,
+    },
+    equipment: String,
+    notes: String,
+    intensity: String,
+    restPeriod: {
+        type: String, // Format: "minutes:seconds"
+        default: "0:00",
+    },
+    targetMuscles: [String],
+    status: {
+        type: String,
+        enum: ['completed', 'skipped', 'pending'],
+        default: 'pending',
+    },
+});
+
+const WorkoutSchema = new mongoose.Schema({
+    routineName: {
+        type: String,
+        required: true,
+    },
+    exercises: [ExerciseSchema],
     date: {
         type: Date,
         default: Date.now,

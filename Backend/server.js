bc0344c1d89e93bf53b56523c8d1f4d5cfe22fe3
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2023-09-17 04:29:24 -0400

    Implemented data fetching and improved styling in Dashboard.jsx

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const workoutRoutes = require('./routes/workoutRoutes');
+const cors = require('cors');
+
 require('colors');
 
 // Load environment variables
@@ -10,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 // Mount the workout routes
+app.use(cors());
 app.use('/api/workouts', workoutRoutes);
+
 
 
 // Connect to MongoDB
--- frontend/src/Components/Dashboard/Dashboard.jsx
+++ frontend/src/Components/Dashboard/Dashboard.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 
 const DashboardContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: var(--background);
-  color: var(--headline);
+  background: linear-gradient(to bottom, #f9f9f9, #e0e0e0);
+  padding: 2rem;
 `;
 
 const DashboardHeader = styled.h1`
   font-size: 2.5rem;
+  color: var(--secondary);
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
+  margin-bottom: 1rem;
 `;
 
 const DashboardContent = styled.div`
@@ -20,14 +24,46 @@ const DashboardContent = styled.div`
   align-items: center;
 `;
 
-
+const WorkoutCard = styled.div`
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  padding: 1rem;
+  margin: 0.5rem 0;
+  width: 100%;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+`;
 
 const Dashboard = () => {
+  const [workouts, setWorkouts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/workouts')
+      .then(response => {
+        setWorkouts(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('There was an error fetching data: ', error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <DashboardContainer>
-      <DashboardHeader>Your Dashboard</DashboardHeader>
+      <DashboardHeader>Workout Dashboard</DashboardHeader>
       <DashboardContent>
-        {/* Your main content here */}
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          workouts.map((workout, index) => (
+            <WorkoutCard key={index}>
+              <h2>{workout.routineName}</h2>
+              {/* Additional workout details can go here */}
+            </WorkoutCard>
+          ))
+        )}
       </DashboardContent>
     </DashboardContainer>
   );
